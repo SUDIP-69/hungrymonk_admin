7fbdfd31f4f6a90cc6272530f9d459ae94f835e8
SUDIP-69 <csudip2311@gmail.com>
2024-07-04 23:39:28 +0530

    publish

---
+++ src/pages/api/login.js
@@ -0,0 +1,40 @@
+import connDB from "../../.././middleware/connDB";
+import User from '../../.././model/User'
+import bcrypt from 'bcrypt'
+var jwt = require('jsonwebtoken')
+
+const handler = async (req, res) => {
+  try {
+    if (req.method == "POST") {
+      const { username, password } = req.body;
+      console.log(username, password);
+      if (username) {
+        const users = await User.findOne({ username });
+        console.log(users);
+        if (users) {
+          const ismatch = await bcrypt.compare(password, users.password);
+          console.log(ismatch);
+          if (ismatch) {
+              const token =jwt.sign({name:users.name},'qwerty')
+              console.log(token);
+            res.json({ success: true,token:token });
+          } else {
+            res.json({ success: false,token:null });
+          }
+        } else {
+          res.json({ success: false });
+        }
+      }else{
+        res.status(201).json({ success: false, token:null });
+      }
+
+      
+    } else {
+      res.status(201).json({ success: false, token:null });
+    }
+  } catch (error) {
+    res.status(201).json({ success: false, token:null });
+  }
+};
+
+export default connDB(handler);
---
+++ src/pages/api/register.js
@@ -0,0 +1,23 @@
+import connDB from "../../.././middleware/connDB";
+import User from '../../.././model/User'
+import bcrypt from 'bcrypt'
+
+const handler =async (req, res) => {
+  try {
+    if (req.method == "POST") {
+      const { username, password, email, name, phoneNo,restaurantid } = req.body;
+
+    console.log(req.body);
+      const passwordhashed = await bcrypt.hash(password, 10)
+      const u = new User({ username, password:passwordhashed, email, phoneNo,name,restaurantid } );
+      const u1=await u.save();
+      res.status(201).json({ success: true, data: u1 });
+    } else {
+      res.status(501).json({ success: false, data: "no data" });
+    }
+  } catch (error) {
+    res.status(501).json({ success: false, data: "no data" });
+  }
+};
+
+export default connDB(handler);
